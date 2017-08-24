84b38f1512507863b8452879c01f0963afc69877
Alisson Wilker <alissonwilker@gmail.com>
2017-08-24 02:42:15 -0300

    Adicionando teste de integração com Arquillian e Wildfly Gerenciado.

---
+++ src/integration-test/java/br/org/libros/biblioteca/model/business/facade/BibliotecaBusinessFacadeITCase.java
@@ -0,0 +1,57 @@
+package br.org.libros.biblioteca.model.business.facade;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.org.libros.biblioteca.dto.BibliotecaDto;
+import br.org.libros.comum.exception.EntidadeJaExisteExcecao;
+
+/**
+ * Classe de teste que testa o comportamento da fachada negocial do módulo
+ * Biblioteca.
+ *
+ */
+@RunWith(Arquillian.class)
+public class BibliotecaBusinessFacadeITCase {
+	@Inject
+	private BibliotecaBusinessFacade bibliotecaBusinessFacade;
+
+	@Deployment
+	public static Archive<?> criarArquivoTeste() {
+		Archive<?> arquivoTeste = ShrinkWrap.create(WebArchive.class, "librosTesteIntegracao.war")
+				.addPackages(true, "br.org.libros").addAsResource("META-INF/persistence.xml").addAsLibrary(new File(
+						"target/libros/WEB-INF/lib/mapstruct-jdk8-1.1.0.Final.jar"));
+
+		return arquivoTeste;
+	}
+
+	/**
+	 * Test method for
+	 * {@link br.org.libros.comum.model.business.facade.AbstractBusinessFacade#adicionar(java.lang.Object)}.
+	 */
+	@Test
+	public void testAdicionar() {
+		BibliotecaDto bibliotecaDto = new BibliotecaDto("nome1");
+		BibliotecaDto bibliotecaDtoResposta;
+		try {
+			bibliotecaDtoResposta = bibliotecaBusinessFacade.adicionar(bibliotecaDto);
+			Assert.assertEquals(bibliotecaDto.getNome(), bibliotecaDtoResposta.getNome());
+			Assert.assertNotNull(bibliotecaDtoResposta.getId());
+		} catch (EntidadeJaExisteExcecao e) {
+			fail("Entidade já existe.");
+		}
+	}
+
+}

26c5db4a5a95a32c68706ccc0f86fc26fbc5e69a
Alisson Wilker <alissonwilker@gmail.com>
2017-09-05 13:00:50 -0300

    Simplificando Mapper de Biblioteca e BibliotecaBusinessFacade.

--- src/main/java/br/org/libros/biblioteca/dto/mapper/IBibliotecaMapper.java
+++ src/main/java/br/org/libros/biblioteca/dto/mapper/IBibliotecaMapper.java
@@ -1,36 +1,18 @@
 package br.org.libros.biblioteca.dto.mapper;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import br.org.libros.biblioteca.dto.BibliotecaDto;
-import br.org.libros.biblioteca.dto.LivroDto;
 import br.org.libros.biblioteca.model.persistente.entity.Biblioteca;
-import br.org.libros.biblioteca.model.persistente.entity.Livro;
+import br.org.libros.comum.dto.mapper.IGenericMapper;
 
 /**
  * 
  * @see br.org.libros.comum.dto.mapper.IGenericMapper
  */
-@Mapper
-public interface IBibliotecaMapper {
+@Mapper(uses = ILivroMapper.class)
+public interface IBibliotecaMapper extends IGenericMapper<Biblioteca, BibliotecaDto> {
 	IBibliotecaMapper INSTANCE = Mappers.getMapper(IBibliotecaMapper.class);
 
-	BibliotecaDto converterBibliotecaParaBibliotecaDto(Biblioteca entidade);
-
-	Biblioteca converterBibliotecaDtoParaBiblioteca(BibliotecaDto dto);
-
-	List<BibliotecaDto> converterBibliotecasParaBibliotecasDto(List<Biblioteca> entidades);
-
-	List<Biblioteca> converterBibliotecasDtoParaBibliotecas(List<BibliotecaDto> dtos);
-
-	LivroDto converterLivroParaLivroDto(Livro entidade);
-
-	Livro converterLivroDtoParaLivro(LivroDto dto);
-
-	List<LivroDto> converterLivrosParaLivrosDto(List<Livro> entidades);
-
-	List<Livro> converterLivrosDtoParaLivros(List<LivroDto> dtos);
 }
--- src/main/java/br/org/libros/biblioteca/model/business/facade/BibliotecaBusinessFacade.java
+++ src/main/java/br/org/libros/biblioteca/model/business/facade/BibliotecaBusinessFacade.java
@@ -1,18 +1,13 @@
 package br.org.libros.biblioteca.model.business.facade;
 
-import java.util.List;
-
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
 import br.org.libros.biblioteca.dto.BibliotecaDto;
 import br.org.libros.biblioteca.dto.mapper.IBibliotecaMapper;
 import br.org.libros.biblioteca.model.persistente.entity.Biblioteca;
-import br.org.libros.comum.exception.EntidadeJaExisteExcecao;
-import br.org.libros.comum.exception.EntidadeNaoEncontradaExcecao;
-import br.org.libros.comum.model.business.IBusiness;
 import br.org.libros.comum.model.business.facade.AbstractBusinessFacade;
 
 /**
@@ -26,48 +21,9 @@ public class BibliotecaBusinessFacade extends AbstractBusinessFacade<Biblioteca,
 
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	private IBusiness<Biblioteca, Integer> business;
-
-	protected IBibliotecaMapper mapper = IBibliotecaMapper.INSTANCE;
-
-	@Override
-	public BibliotecaDto adicionar(BibliotecaDto dto) throws EntidadeJaExisteExcecao {
-		return mapper.converterBibliotecaParaBibliotecaDto(
-				business.adicionar(mapper.converterBibliotecaDtoParaBiblioteca(dto)));
-	}
-
-	@Override
-	public List<BibliotecaDto> listar() {
-		return mapper.converterBibliotecasParaBibliotecasDto(business.listar());
-	}
-
-	@Override
-	public void remover(BibliotecaDto dto) throws EntidadeNaoEncontradaExcecao {
-		business.remover(mapper.converterBibliotecaDtoParaBiblioteca(dto));
-	}
-
-	@Override
-	public BibliotecaDto atualizar(BibliotecaDto dto) throws EntidadeNaoEncontradaExcecao, EntidadeJaExisteExcecao {
-		return mapper.converterBibliotecaParaBibliotecaDto(
-				business.atualizar(mapper.converterBibliotecaDtoParaBiblioteca(dto)));
-	}
-
-	@Override
-	public BibliotecaDto recuperar(Integer chavePrimaria) throws EntidadeNaoEncontradaExcecao {
-		return mapper.converterBibliotecaParaBibliotecaDto(business.recuperar(chavePrimaria));
-	}
-
-	@Override
-	public void remover(Integer chavePrimaria) throws EntidadeNaoEncontradaExcecao {
-		business.remover(chavePrimaria);
-	}
-
-	@Override
-	public BibliotecaDto atualizar(Integer chavePrimaria, BibliotecaDto dto)
-			throws EntidadeNaoEncontradaExcecao, EntidadeJaExisteExcecao {
-		return mapper.converterBibliotecaParaBibliotecaDto(
-				business.atualizar(chavePrimaria, mapper.converterBibliotecaDtoParaBiblioteca(dto)));
+	@PostConstruct
+	public void init() {
+		mapper = IBibliotecaMapper.INSTANCE;
 	}
 
 }
